b99737f9ade5c831b6e73cbbc3b08a72c6425b14
CodeBoyQ <codeboyq@originalcoder.nl>
2018-06-27 21:24:43 +0200

    Ignore Test

--- src/test/java/com/codeboyq/AdminiMover/MainAppTest.java
+++ src/test/java/com/codeboyq/AdminiMover/MainAppTest.java
@@ -2,12 +2,12 @@ package com.codeboyq.AdminiMover;
 
 import java.io.File;
 
-import org.junit.Test;
+import org.junit.Ignore;
 
 public class MainAppTest
 {
 
-    @Test
+    @Ignore("Because this test triggers the Main method which asks for user input")
     public void testMainApp() throws Exception {
     	File inputFile = File.createTempFile("Invoice", ".pdf");
         String[] args = {inputFile.getPath()};
